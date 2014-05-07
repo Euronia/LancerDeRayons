c38fa8eebc08ccd0bb847ebdd60a47952e0e8fcb
Euronia <mouton.thibault@gmail.com>
2014-05-07 14:33:26 +0200

    Added 3DPoint files

---
+++ 3DPoint.h
@@ -0,0 +1,20 @@
+#ifndef 3DPOINT_H
+#define 3DPOINT_H
+
+class 3DPoint {
+
+public :
+    3DPoint();
+    3DPoint(int x, int y, int z);
+    int x ;
+    int y ;
+    int z ;
+
+protected :
+
+private :
+
+
+};
+
+#endif
