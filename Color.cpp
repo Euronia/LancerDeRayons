df2b25e1d296a038e644f57ad25968aeaaa1b5ad
ArcanicMojo <heldvar@gmail.com>
2014-05-14 09:31:49 +0200

    Updated color files (add and multiply functions)

--- Color.cpp
+++ Color.cpp
@@ -9,32 +9,54 @@ Color::Color()
 {
 }
 
-Color::Color(float x, float y, float z)
+Color::Color(float r, float g, float b)
 {
-    if(x>=0.0 && x<=1.0)
+    if(r>=0.0 && r<=1.0)
     {
-        r = x;
+        this->r = r;
     }
     else
     {
-        r = 1.0;
+        this->r = 1.0;
     }
 
-    if(y>=0.0 && y<=1.0)
+    if(g>=0.0 && g<=1.0)
     {
-        g = y;
+        this->g = g;
     }
     else
     {
-        g = 1.0;
+        this->g = 1.0;
     }
 
-    if(z>=0.0 && z<=1.0)
+    if(b>=0.0 && b<=1.0)
     {
-        b = z;
+        this->b = b;
     }
     else
     {
-        b = 1.0;
+        this->b = 1.0;
     }
+}
+
+Color Color::addColors(const Color& c1, const Color& c2)
+{
+    Color colorResult;
+
+    colorResult.r = c1.r + c2.r;
+    colorResult.g = c1.g + c2.g;
+    colorResult.b = c1.b + c2.b;
+
+    return colorResult;
+}
+
+Color Color::multiplyColors(const Color& c1, const Color& c2)
+{
+    Color colorResult;
+
+    colorResult.r = c1.r * c2.r;
+    colorResult.g = c1.g * c2.g;
+    colorResult.b = c1.b * c2.b;
+
+    return colorResult;
 }
