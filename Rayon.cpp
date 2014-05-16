2723ebd60b3342817a9f0b5044a530798808e53f
ArcanicMojo <heldvar@gmail.com>
2014-05-16 09:52:39 +0200

    Added colorCarried and getDistantPoint to Rayon

--- Rayon.cpp
+++ Rayon.cpp
@@ -2,10 +2,20 @@
 #include "Rayon.h"
 
 
- Rayon::Rayon(const Point& origin, const Vector& direction)
+ Rayon::Rayon(const Point& origin, const Vector& direction, const Color& colorCarried)
  :
      originPoint(origin) ,
-     directionalVector(direction)
-
+     directionalVector(direction),
+     colorCarried(colorCarried)
     {
     }
+
+Point Rayon::getDistantPoint(float k)
+{
+    Point distantPoint;
+    distantPoint.x = this->originPoint.x + k*this->directionalVector.getX();
+    distantPoint.y = this->originPoint.y + k*this->directionalVector.getY();
+    distantPoint.z = this->originPoint.z + k*this->directionalVector.getZ();
+    return(distantPoint);
+}
+
