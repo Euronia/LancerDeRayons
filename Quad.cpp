e0a259d43212fd8becda8469377c7a2379feb1ef
ArcanicMojo <heldvar@gmail.com>
2014-06-04 14:50:31 +0200

    Updated Quad class

--- Quad.cpp
+++ Quad.cpp
@@ -1,9 +1,7 @@
-#include <iostream>
 #include <cmath>
 #include "Quad.h"
 #include "Hit.h"
 
-using namespace std;
 
 Quad::Quad(const Point& a, const Point& b, const Point& c, const Point& d)
 :
@@ -21,11 +19,13 @@ bool Quad::intersect(const Rayon &rayon, float& dist) const
 
 bool Quad::intersect(const Rayon& rayon, Hit& hit, float& dist) const
 {
-    Vector OA(a.x-rayon.getOrigin().x, a.y-rayon.getOrigin().y,a.z-rayon.getOrigin().z);
+    Vector OA(this->a.x-rayon.getOrigin().x, this->a.y-rayon.getOrigin().y, this->a.z-rayon.getOrigin().z);
     Vector OP;
     Vector ab(this->b.x-this->a.x, this->b.y-this->a.y, this->b.z-this->a.z);
     Vector ad(this->d.x-this->a.x, this->d.y-this->a.y, this->d.z-this->a.z);
-    hit.setNormal(crossProduct(ab,ad));
+    Vector norm(crossProduct(ab,ad));
+    norm.normalize();
+    hit.setNormal(norm);
     OP = rayon.getDirect()*(dotProduct(OA, hit.getNormal())/dotProduct(rayon.getDirect(), hit.getNormal()));
     Point p = rayon.getDistantPoint(OP.getNorm());
     Vector bc(this->c.x-this->b.x, this->c.y-this->b.y, this->c.z-this->b.z);
@@ -35,11 +35,17 @@ bool Quad::intersect(const Rayon& rayon, Hit& hit, float& dist) const
     Vector bp(p.x-this->b.x, p.y-this->b.y, p.z-this->b.z);
     Vector cp(p.x-this->c.x, p.y-this->c.y, p.z-this->c.z);
     Vector dp(p.x-this->d.x, p.y-this->d.y, p.z-this->d.z);
+    Vector AB_AP = crossProduct(ab,ap);
+    Vector BC_BP = crossProduct(bc,bp);
+    Vector CD_CP = crossProduct(cd,cp);
+    Vector DA_DP = crossProduct(da,dp);
+    float PSB = dotProduct(AB_AP,BC_BP);
+    float PSC = dotProduct(BC_BP,CD_CP);
+    float PSD = dotProduct(CD_CP,DA_DP);
 
-    if (abs(dotProduct(crossProduct(ab,ap),crossProduct(bc,bp))) == crossProduct(ab,ap).getNorm()*crossProduct(bc,bp).getNorm() &&
-        abs(dotProduct(crossProduct(bc,bp),crossProduct(cd,cp))) == crossProduct(bc,bp).getNorm()*crossProduct(cd,cp).getNorm() &&
-        abs(dotProduct(crossProduct(cd,cp),crossProduct(da,dp))) == crossProduct(cd,cp).getNorm()*crossProduct(da,dp).getNorm())
+    if (PSB*PSC >= 0 && PSC*PSD >=0)
     {
+        hit.setImpactPoint(p);
         hit.setU(0.5);
         hit.setV(0.5);
         return true;
